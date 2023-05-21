f4fe69f085567e736444a7b82e6f51b5678a535a
LucaCamp <campanella440@gmail.com>
2023-05-22 00:10:59 +0200

    script modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,96 @@
-let counter = 0
-let number = document.getElementById("number");
 
-const buttonPlus = document.getElementById("plus");
-const buttonMinus = document.getElementById("minus");
-const buttonReset = document.getElementById("reset");
-const buttonMinus100= document.getElementById("minus100");
-const buttonPlus100 = document.getElementById("plus100");
-const buttonRandom = document.getElementById("random");
+//create counter div
+
+let counterDiv = document.createElement("div");
+document.getElementById("number-container").appendChild(counterDiv);
+
+let counter = 0
+
+counterDiv.innerHTML = counter ;
+counterDiv.classList.add("number" , "text-danger") ;
+
+
+//create buttons 
+
+
+let minusBtn = document.createElement("button");
+document.getElementById("btnRow1").appendChild(minusBtn);
+minusBtn.classList.add("btn","btn-lg" ,"btn-success");
+minusBtn.id = "minusBtn";
+minusBtn.innerHTML = "-1"
+
+ let resetBtn = document.createElement ("button");
+document.getElementById("btnRow1").appendChild(resetBtn);
+resetBtn.classList.add("btn","btn-lg" ,"btn-success");
+ resetBtn.id = "reset";
+resetBtn.innerHTML = "reset"
+
+let plusBtn = document.createElement ("button");
+document.getElementById ("btnRow1").appendChild(plusBtn);
+plusBtn.classList.add("btn","btn-lg" ,"btn-success");
+plusBtn.id = "plus";
+plusBtn.innerHTML = "+1"
+
+
+let minus100Btn = document.createElement ("button");
+ document.getElementById ("btnRow2").appendChild(minus100Btn);
+ minus100Btn.classList.add("btn","btn-lg" ,"btn-success");
+minus100Btn.id = "plus";
+ minus100Btn.innerHTML = "-100"
+
+ let randomBtn = document.createElement ("button");
+ document.getElementById ("btnRow2").appendChild(randomBtn);
+randomBtn.classList.add("btn","btn-lg" ,"btn-success");
+randomBtn.id = "random";
+random.innerHTML = "random"
+
+ let plus100Btn = document.createElement ("button");
+ document.getElementById ("btnRow2").appendChild(plus100Btn);
+plus100Btn.classList.add("btn","btn-lg" ,"btn-success");
+plus100Btn.id = "plus100";
+plus100Btn.innerHTML = "+100"
+
+
+
+//event listener Click
+//Event delegation
+
+let buttons = document.querySelector(".buttons");
+
+buttons.addEventListener ("click", function(event) {
+  if (event.target.tagName === "BUTTON") {
+     switch (event.target) {
+      case plusBtn: 
+      add();
+      break;
+
+      case minusBtn: 
+      subtract();
+      break;
+
+      case resetBtn: 
+      reset();
+      break;
+
+      case minus100Btn: 
+      subtract100();
+      break;
+
+      case plus100Btn: 
+      add100();
+      break;
+      
+      case randomBtn: 
+      randomNumber();
+      break;
+     }
+  }
+})
+
+//function for button 
 
 function updateCounter (){
-  number.innerHTML = counter;
+  counterDiv.innerHTML = counter;
 }
 
 function add(){
@@ -42,14 +123,3 @@ function randomNumber(){
 }
 
 
-buttonPlus.addEventListener("click", add);
-
-buttonMinus.addEventListener("click", subtract);
-
-buttonReset.addEventListener("click", reset);
-
-buttonMinus100.addEventListener("click", subtract100);
-
-buttonPlus100.addEventListener("click", add100);
-
-buttonRandom.addEventListener("click",randomNumber);
